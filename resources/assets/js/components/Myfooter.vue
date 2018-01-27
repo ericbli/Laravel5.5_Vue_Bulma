cde206ffeee81b3de5b41c6eeb03c583b7bbaf1a
ericbli <ericbli.se@gmail.com>
2018-01-27 19:32:50 +0100

    version 0.1

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,13 @@
+<template >
+  <footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Laravel vue Phonebook</strong> by <a href="https://jgthms.com">Eric Li</a>. The source code is licensed
+        <a href="https://github.com/ericbli/Laravel5.5_Vue_Bulma">MIT</a>. The website content
+        is in Github <a href="https://github.com/ericbli/Laravel5.5_Vue_Bulma">version 1.0</a>.
+      </p>
+    </div>
+  </div>
+</footer>
+</template>
